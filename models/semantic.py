54ceba467f6e4bdf0eb23e2b465d356a4c3e6c81
claudiocamolese <claudiocamolese@gmail.com>
2025-05-08 11:55:29 +0200

    two strings similarity

--- models/semantic.py
+++ models/semantic.py
@@ -1,24 +1,25 @@
-import torch
-from model import BaseModel
+class SentenceSimilarity:
+    def __init__(self, model_name,  quantization_config, device):
+        self.model_name = model_name
+        self.quantization_config = quantization_config
+        self.device = device
+        
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
+        self.model = AutoModel.from_pretrained(self.model_name).to(self.device)
+        self.model.eval()  # Imposta il modello in modalità valutazione
 
-class SentenceSimilarity(BaseModel):
-    def __init__(self, model_name, quantization_config, device):
-        super().__init__(model_name, quantization_config, device)
-
-    # Funzione per ottenere gli embedding delle frasi
-    def get_embeddings(self, sentences):
-        # Tokenizza le frasi
-        inputs = self.tokenizer(sentences, padding=True, truncation=True, return_tensors="pt")
+    def get_embeddings(self, sentence):
+        # Tokenizza e calcola gli embedding
+        inputs = self.tokenizer(sentence, padding=True, truncation= False, return_tensors="pt").to(self.device)
         with torch.no_grad():
-            # Calcola l'embedding usando l'output dell'ultima hidden layer
-            embeddings = self.model(**inputs).last_hidden_state.mean(dim=1)
+            outputs = self.model(**inputs)
+            embeddings = outputs.last_hidden_state.mean(dim=1)  # Media dei token
         return embeddings
 
-    def calculate_cosine_similarity(self, source_sentence, sentences):
-        # Ottieni gli embedding delle frasi
-        source_embedding = self.get_embeddings([source_sentence])
-        sentence_embeddings = self.get_embeddings(sentences)
+    def similarity_between_two_sentences(self, sentence1, sentence2):
+        # Funzione che accetta esattamente due frasi
+        emb1 = self.get_embeddings([sentence1])
+        emb2 = self.get_embeddings([sentence2])
+        cosine_sim = F.cosine_similarity(emb1, emb2)
+        return float(cosine_sim.item())  # Restituisce un float
 
-        cosine_sim = torch.nn.functional.cosine_similarity(source_embedding, sentence_embeddings)
-
-        return cosine_sim.numpy()  
